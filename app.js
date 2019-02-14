f8323bb1499fc8b8888c1084900e8cee37eb0ed2
J <j@jgarcia.jp>
2019-02-14 12:11:13 +0900

    done

---
+++ app.js
@@ -0,0 +1,56 @@
+const youtubedl = require("ytdl-core")
+const fs = require("fs")
+
+const arg = require('arg')({
+    '--manual': String,
+    '-m' : '--manual',
+})
+
+let vidURL = false
+if(arg._[0] !== undefined){
+    vidURL = arg._[0]
+} else {
+    console.log("please enter video url")
+}
+
+const DL_DIR = "dl"
+
+;(async () => {
+
+    console.log("getting info..")
+
+    const info = await youtubedl.getInfo( vidURL, [] )
+
+    const title = info.title.replace(/ /g,"-")
+    const filename = title + ".mp4"
+
+    console.log("downloading to "+filename)
+
+    if ( !fs.existsSync(DL_DIR) ) fs.mkdirSync(DL_DIR)
+
+    youtubedl( vidURL )
+        .pipe(fs.createWriteStream( DL_DIR + "/" + filename ))
+
+        // youtubedl.getInfo(url, options, function(err, info) {
+        //   if (err) throw err;
+         
+        //   console.log('id:', info.id);
+        //   console.log('title:', info.title);
+        //   console.log('url:', info.url);
+        //   console.log('thumbnail:', info.thumbnail);
+        //   console.log('description:', info.description);
+        //   console.log('filename:', info._filename);
+        //   console.log('format id:', info.format_id);
+        // })
+             
+        //     let video = youtubedl( vid, ['--format=18'], { cwd: __dirname } );
+        //     // Will be called when the download starts.
+        //     video.on('info', function(info) {
+        //       console.log('Download started')
+        //       console.log('filename: ' + info.filename)
+        //       console.log('size: ' + info.size)
+        //     })
+             
+        //     video.pipe(fs.createWriteStream(DL_DIR + '/' info.filename + '.mp4'));
+
+})()
